5d0b5230fd2d3eafbb67f88e2080722fb26a6b61
Wonder Oladunjoye <itz.wondee@gmail.com>
2024-09-15 17:47:57 -0500

    Commit

---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            if (entity == null)
+            {
+                throw new Exception("NotFound");
+            }
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            if (query.Count() < 1)
+            {
+                throw new Exception("NotFound");
+            }
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            if (query.Count() < 1)
+            {
+                throw new Exception("NotFound");
+            }
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new Exception("NotFound");
+            }
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
---
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+                return View(objCategoryList);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Category NotFound";
+                return RedirectToAction("Home/Index");
+            }
+        }
+
+        //[Route("Category/Create/{id}")]
+        public IActionResult Create()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Category NotFound";
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost] // We are sending information to the server.
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Category/Edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                Category categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+                return View(categoryFromDb);
+            }
+            catch (Exception ex) 
+            {
+                TempData["error"] = "Category NotFound";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost] // We are sending information to the server.
+        [Route("Category/Edit/{id}")]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [Route("Category/Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                Category categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+                return View(categoryFromDb);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Category NotFound";
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost, ActionName("Delete")] // We are sending information to the server.
+        [Route("Category/Delete/{id}")]
+        public IActionResult DeletePOST(int id)
+        {
+            Category obj = _unitOfWork.Category.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
